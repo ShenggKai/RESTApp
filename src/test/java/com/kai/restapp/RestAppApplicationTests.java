0eb0a1933b9f01172c87109e55ad694c218fe31c
shengkai <maimemdada@gmail.com>
2024-07-26 01:50:50 +0700

    Init REST API project

---
+++ src/test/java/com/kai/restapp/RestAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kai.restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
